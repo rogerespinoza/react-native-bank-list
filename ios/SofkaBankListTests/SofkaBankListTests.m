12b4f1f4db5839c6e13d7d52d2e27e8e964fc5aa
Roger Espinoza <rj.espinoza44@gmail.com>
2024-05-13 13:26:36 -0400

    bugfix - directory name

--- ios/SofkaBankListTests/SofkaBankListTests.m
+++ ios/SofkaBankListTests/SofkaBankListTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface SoftkaBankListTests : XCTestCase
+@interface SofkaBankListTests : XCTestCase
 
 @end
 
-@implementation SoftkaBankListTests
+@implementation SofkaBankListTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
