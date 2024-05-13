1ee45b23f2407987b1552f595ebfb8bd4baadc61
Roger Espinoza <rj.espinoza44@gmail.com>
2024-05-13 17:36:11 -0400

    feature - test cases

---
+++ __tests__/src/components/button.test.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react-native';
+import { Button } from '../../../src/components';
+
+describe('Button', () => {
+  const handlePress = jest.fn();
+
+  it('renders primary button correctly', () => {
+    const { getByText } = render(
+      <Button label="Press Me 1" onPress={handlePress} />,
+    );
+    expect(getByText('Press Me 1')).toBeTruthy();
+  });
+
+  it('renders secondary button correctly', () => {
+    const { getByText } = render(
+      <Button label="Press Me 2" onPress={handlePress} />,
+    );
+    expect(getByText('Press Me 2')).toBeTruthy();
+  });
+
+  it('triggers onPress when pressed', () => {
+    const { getByText } = render(
+      <Button label="Press Me" onPress={handlePress} />,
+    );
+
+    fireEvent.press(getByText('Press Me'));
+    expect(handlePress).toHaveBeenCalled();
+  });
+});
