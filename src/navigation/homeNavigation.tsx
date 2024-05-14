0935167e95903bb1bda37fee032f53c8dc96fb02
Roger Espinoza <rj.espinoza44@gmail.com>
2024-05-14 01:18:04 -0400

    feature - nav bar

--- src/navigation/homeNavigation.tsx
+++ src/navigation/homeNavigation.tsx
@@ -20,19 +20,23 @@ export function HomeNavigation() {
           name={homeRoutes.productList}
           component={ProductListScreen}
           initialParams={{}}
+          options={{ title: 'BANCO' }}
         />
         <Stack.Screen
           name={homeRoutes.productDetail}
           component={ProductDetailScreen}
           initialParams={{}}
+          options={{ title: 'BANCO' }}
         />
         <Stack.Screen
           name={homeRoutes.addProduct}
           component={AddProductScreen}
+          options={{ title: 'BANCO' }}
         />
         <Stack.Screen
           name={homeRoutes.editProduct}
           component={EditProductScreen}
+          options={{ title: 'BANCO' }}
         />
       </Stack.Navigator>
     </NavigationContainer>
